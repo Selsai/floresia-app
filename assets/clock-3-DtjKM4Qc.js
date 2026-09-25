145068147ff52f3653a11c11657cf169c86a9483
Selsai <Selsai@users.noreply.github.com>
2026-09-25 20:24:02 +0000

    deploy: 7a20a4b6344716d95a6666c0a34cdea9330ebe93

---
+++ assets/clock-3-DtjKM4Qc.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-l1CyDpXD.js";const c={name:"clock-3",size:24,node:[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"M12 6v6h4",key:"135r8i"}]]};c.node;const a=e(c);export{a as C};
